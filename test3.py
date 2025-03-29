dda0beda303c30cb163ffc9fb8a0ded309cb3f97
SummitSeoul2025 <summitseouldemo@gmail.com>
2025-03-29 22:18:53 +0900

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,11 @@
+def calculate_total(a, b, c):
+    unused_var = 10
+    return a + b
+
+def nested_conditions(x):
+    if x > 0:
+        if x < 10:
+            if x % 2 == 0:
+                if x > 5:
+                    return True
+    return False
